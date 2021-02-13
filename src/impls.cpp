983a841a71a3f63611034060d946f8613b7bf824
E-gy <balakhelisey@gmail.com>
2021-02-13 22:52:16 +0100

    templates go to headers :/

--- src/impls.cpp
+++ src/impls.cpp
@@ -1,40 +1,7 @@
 #include "impls.hpp"
 
-#include <thread>
-
-#include "engine.hpp"
 
 bool RangeGenerator::done() const { return c >= e; }
 std::variant<std::shared_ptr<FutureBase>, int> RangeGenerator::resume([[maybe_unused]] const Yengine* eng){
 	return c++;
-}
-
-/*class UnFuture : public Future<void> {
-	public:
-		FutureState s = FutureState::Running;
-		FutureState state(){ return s; }
-		std::optional<void> result(){
-			return std::optional(void);
-		}
-};*/
-
-template<typename T> class OutsideFuture : public Future<T> {
-	public:
-		OutsideFuture(){}
-		FutureState s = FutureState::Running;
-		FutureState state(){ return s; }
-		std::optional<T> r;
-		std::optional<T> result(){ return r; }
-};
-
-template<typename T> std::shared_ptr<Future<T>> asyncSleep(Yengine* engine, unsigned ms, T ret){
-	std::shared_ptr<OutsideFuture<T>> f(new OutsideFuture<T>());
-	std::thread th([engine, ms, ret](std::shared_ptr<OutsideFuture<T>> f){
-		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-		f->s = FutureState::Completed;
-		f->r = ret;
-		engine->notify(f);
-	}, f);
-	th.detach();
-	return f;
 }
--- src/impls.hpp
+++ src/impls.hpp
@@ -12,6 +12,35 @@ class RangeGenerator : public AGenerator<int> {
 		std::variant<std::shared_ptr<FutureBase>, int> resume(const Yengine* eng);
 };
 
-class Yengine;
+#include <thread>
+#include "engine.hpp"
 
-template<typename T> std::shared_ptr<Future<T>> asyncSleep(Yengine* engine, unsigned ms, T ret);
+/*class UnFuture : public Future<void> {
+	public:
+		FutureState s = FutureState::Running;
+		FutureState state(){ return s; }
+		std::optional<void> result(){
+			return std::optional(void);
+		}
+};*/
+
+template<typename T> class OutsideFuture : public Future<T> {
+	public:
+		OutsideFuture(){}
+		FutureState s = FutureState::Running;
+		FutureState state(){ return s; }
+		std::optional<T> r;
+		std::optional<T> result(){ return r; }
+};
+
+template<typename T> std::shared_ptr<Future<T>> asyncSleep(Yengine* engine, unsigned ms, T ret){
+	std::shared_ptr<OutsideFuture<T>> f(new OutsideFuture<T>());
+	std::thread th([engine, ms, ret](std::shared_ptr<OutsideFuture<T>> f){
+		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+		f->s = FutureState::Completed;
+		f->r = ret;
+		engine->notify(std::dynamic_pointer_cast<Future<T>>(f));
+	}, f);
+	th.detach();
+	return f;
+}
