bdf048d5a7038ac0adcb533ec5e9a9c5fc0a1fb0
E-gy <balakhelisey@gmail.com>
2021-02-16 22:07:53 +0100

    🔨

--- src/result.hpp
+++ src/result.hpp
@@ -10,8 +10,8 @@ template<typename S, typename E> class result {
 		result(const E& err) : res(err) {}
 		bool isOk() const { return res.index() == 0; }
 		bool isError() const { return res.index() == 1; }
-		std::optional<S> ok() const { if(auto r = std::get_if<S>(res)) return *r; else return std::nullopt; }
-		std::optional<E> error() const { if(auto r = std::get_if<E>(res)) return *r; else return std::nullopt; }
+		std::optional<S> ok() const { if(auto r = std::get_if<S>(&res)) return *r; else return std::nullopt; }
+		std::optional<E> error() const { if(auto r = std::get_if<E>(&res)) return *r; else return std::nullopt; }
 };
 template<typename S> class result<S, void> {
 	std::optional<S> okay;
@@ -31,3 +31,7 @@ template<typename E> class result<void, E> {
 		bool isError() const { return err.has_value(); }
 		std::optional<E> error() const { return err; }
 };
+template<typename S, typename E> result<S, E> ROk(const S& ok){ return result<S, E>(ok); }
+template<typename E> result<void, E> ROk(){ return result<void, E>(); }
+template<typename S, typename E> result<S, E> RError(const E& err){ return result<S, E>(err); }
+template<typename S> result<S, void> RError(){ return result<S, void>(); }
