200dfa8d70c949109c91b64b9c0cf305e50b4951
E-gy <balakhelisey@gmail.com>
2021-02-16 17:24:05 +0100

    Interlaced IO platform impls

--- src/io.cpp
+++ src/io.cpp
@@ -3,196 +3,37 @@
 #include <array>
 #include "impls.hpp"
 
-//Common
-namespace yasync::io {
-
-Yengine* IOYengine::yengine() const { return engine; }
-
-}
-
-#ifdef _WIN32
-
-#include <fileapi.h>
 #include <iostream> //print error debug
 
-namespace yasync::io {
-
-constexpr size_t DEFAULT_BUFFER_SIZE = 4096;
-constexpr unsigned COMPLETION_KEY_SHUTDOWN = 1;
-constexpr unsigned COMPLETION_KEY_IO = 2;
-
-IOYengine::IOYengine(Yengine* e) : engine(e) {
-	ioCompletionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, ioThreads);
-	for(unsigned i = 0; i < ioThreads; i++){
-		std::thread th([this](){ this->iothreadwork(); });
-		th.detach();
-	}
-}
-
-IOYengine::~IOYengine(){
-	for(unsigned i = 0; i < ioThreads; i++) PostQueuedCompletionStatus(ioCompletionPort, 0, COMPLETION_KEY_SHUTDOWN, NULL);
-	CloseHandle(ioCompletionPort);
-}
-
-void PrintLastError(DWORD lerr){
-	LPSTR err;
-	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_IGNORE_INSERTS, NULL, lerr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&err, 0, NULL);
-	std::cerr << "Uh oh - " << err;
-	LocalFree(err);
-	throw std::runtime_error("uh oh :("); //FIXME no! Use. Results.
-}
-
-struct IOCompletionInfo {
-	BOOL status;
-	DWORD transferred;
-	DWORD lerr;
-};
-
-class FileResource : public IAIOResource {
-	enum class Mode {
-		Read, Write
-	};
-	std::weak_ptr<FileResource> slf;
-	struct Trixter {
-		OVERLAPPED overlapped;
-		FileResource* cmon;
-	};
-	Trixter trixter = {};
-	IOYengine* engine;
-	Mode mode;
-	HANDLE file;
-	std::array<char, DEFAULT_BUFFER_SIZE> buffer;
-	std::shared_ptr<OutsideFuture<IOCompletionInfo>> engif;
-	public:
-		friend class IOYengine;
-		FileResource(IOYengine* e, Mode m, const std::string& path) : engine(e), mode(m), buffer(), engif(new OutsideFuture<IOCompletionInfo>()) {
-			trixter.cmon = this;
-			file = CreateFileA(path.c_str(), mode == Mode::Write ? GENERIC_WRITE : GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_FLAG_OVERLAPPED/* | FILE_FLAG_NO_BUFFERING cf https://docs.microsoft.com/en-us/windows/win32/fileio/file-buffering?redirectedfrom=MSDN */, NULL);
-			if(file == INVALID_HANDLE_VALUE) throw std::runtime_error("CreateFile failed :("); //FIXME no?
-			CreateIoCompletionPort(file, e->ioCompletionPort, COMPLETION_KEY_IO, 0);
-		}
-		FileResource(const FileResource& cpy) = delete;
-		FileResource(FileResource&& mov) = delete;
-		~FileResource(){
-			if(file != INVALID_HANDLE_VALUE) CloseHandle(file);
-		}
-		auto setSelf(std::shared_ptr<FileResource> self){
-			return slf = self;
-		}
-		Future<std::vector<char>> read(unsigned bytes){
-			//self.get() == this   exists to memory-lock dangling IO resource to this lambda generator
-			return defer(lambdagen([this, self = slf.lock(), bytes]([[maybe_unused]] const Yengine* engine, bool& done, std::vector<char>& data) -> std::variant<AFuture, something<std::vector<char>>> {
-				if(done) return data;
-				if(engif->s == FutureState::Completed){
-					IOCompletionInfo result = *engif->r;
-					if(!result.status){
-						if(result.lerr == ERROR_HANDLE_EOF){
-							done = true;
-							return data;
-						} else PrintLastError(result.lerr);
-					} else {
-						data.insert(data.end(), buffer.begin(), buffer.begin()+result.transferred);
-						trixter.overlapped.Offset += result.transferred;
-						if(bytes > 0 && (done = data.size() >= bytes)){
-							done = true;
-							return data;
-						}
-					}
-				}
-				engif->s = FutureState::Running;
-				DWORD transferred = 0;
-				while(ReadFile(file, buffer.begin(), bytes == 0 ? buffer.size() : std::min(buffer.size(), bytes - data.size()), &transferred, &trixter.overlapped)){
-					data.insert(data.end(), buffer.begin(), buffer.begin() + transferred);
-					trixter.overlapped.Offset += transferred;
-					if(bytes > 0 && data.size() >= bytes){
-						done = true;
-						return data;
-					}
-				}
-				switch(::GetLastError()){
-					case ERROR_IO_PENDING: break;
-					case ERROR_HANDLE_EOF:
-						done = true;
-						return data;
-					default: PrintLastError(::GetLastError());
-				}
-				return engif;
-			}, std::vector<char>()));
-		}
-		Future<void> write(const std::vector<char>& data){
-			return defer(lambdagen([this, self = slf.lock()]([[maybe_unused]] const Yengine* engine, bool& done, std::vector<char>& data) -> std::variant<AFuture, something<void>> {
-				if(data.empty()) done = true;
-				if(done) return something<void>();
-				if(engif->s == FutureState::Completed){
-					IOCompletionInfo result = *engif->r;
-					if(!result.status) PrintLastError(result.lerr);
-					else {
-						data.erase(data.begin(), data.begin()+result.transferred);
-						trixter.overlapped.Offset += result.transferred;
-						if(data.empty()){
-							done = true;
-							return something<void>();
-						}
-					}
-				}
-				engif->s = FutureState::Running;
-				DWORD transferred = 0;
-				while(WriteFile(file, data.data(), data.size(), &transferred, &trixter.overlapped)){
-					data.erase(data.begin(), data.begin()+transferred);
-					trixter.overlapped.Offset += transferred;
-				}
-				if(::GetLastError() != ERROR_IO_PENDING) PrintLastError(::GetLastError());
-				return engif;
-			}, std::move(data)));
-		}
-};
-
-void IOYengine::iothreadwork(){
-	while(true){
-		IOCompletionInfo inf;
-		ULONG_PTR key;
-		LPOVERLAPPED overl;
-		inf.status = GetQueuedCompletionStatus(ioCompletionPort, &inf.transferred, &key, &overl, INFINITE);
-		if(key == COMPLETION_KEY_SHUTDOWN) break;
-		if(key == COMPLETION_KEY_IO){
-			inf.lerr = GetLastError();
-			auto resource = reinterpret_cast<FileResource::Trixter*>(overl)->cmon;
-			resource->engif->s = FutureState::Completed;
-			resource->engif->r.emplace(inf);
-			engine->notify(std::dynamic_pointer_cast<IFutureT<IOCompletionInfo>>(resource->engif));
-		}
-	}
-}
-
-Resource IOYengine::fileOpenRead(const std::string& path){
-	std::shared_ptr<FileResource> r(new FileResource(this, FileResource::Mode::Read, path));
-	r->setSelf(r);
-	return r;
-}
-Resource IOYengine::fileOpenWrite(const std::string& path){
-	std::shared_ptr<FileResource> r(new FileResource(this, FileResource::Mode::Write, path));
-	r->setSelf(r);
-	return r;
-}
-
-}
-
+#ifdef _WIN32
+#include <fileapi.h>
 #else
-
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h> //for perror reporting
-#include <iostream>
+#endif
+
+constexpr size_t DEFAULT_BUFFER_SIZE = 4096;
+#ifdef _WIN32
+constexpr unsigned COMPLETION_KEY_SHUTDOWN = 1;
+constexpr unsigned COMPLETION_KEY_IO = 2;
+#else
+constexpr unsigned EPOLL_MAX_EVENTS = 64;
+#endif
 
 namespace yasync::io {
 
-constexpr size_t DEFAULT_BUFFER_SIZE = 4096;
-constexpr unsigned EPOLL_MAX_EVENTS = 64;
-
 IOYengine::IOYengine(Yengine* e) : engine(e) {
+	#ifdef _WIN32
+	ioCompletionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, ioThreads);
+	for(unsigned i = 0; i < ioThreads; i++){
+		std::thread th([this](){ this->iothreadwork(); });
+		th.detach();
+	}
+	#else
 	ioEpoll = ::epoll_create1(EPOLL_CLOEXEC);
 	if(ioEpoll < 0) throw std::runtime_error("Initalizing EPoll failed");
 	fd_t pipe2[2];
@@ -203,19 +44,54 @@ IOYengine::IOYengine(Yengine* e) : engine(e) {
 	if(::epoll_ctl(ioEpoll, EPOLL_CTL_ADD, cfdStopReceive, &epm)) throw std::runtime_error("Initalizing close down pipe epoll failed");
 	std::thread th([this](){ this->iothreadwork(); });
 	th.detach();
+	#endif
 }
 
 IOYengine::~IOYengine(){
+	#ifdef _WIN32
+	for(unsigned i = 0; i < ioThreads; i++) PostQueuedCompletionStatus(ioCompletionPort, 0, COMPLETION_KEY_SHUTDOWN, NULL);
+	CloseHandle(ioCompletionPort);
+	#else
 	close(cfdStopSend); //sends EPOLLHUP to receiving end
 	close(cfdStopReceive);
 	//hmmm...
 	close(ioEpoll);
+	#endif
 }
 
+Yengine* IOYengine::yengine() const { return engine; }
+
+#ifdef _WIN32
+void PrintLastError(DWORD lerr){
+	LPSTR err;
+	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_IGNORE_INSERTS, NULL, lerr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&err, 0, NULL);
+	std::cerr << "Uh oh - " << err;
+	LocalFree(err);
+	throw std::runtime_error("uh oh :("); //FIXME no! Use. Results.
+}
+#else
 static void PrintLastError(const std::string& errm){
 	perror(errm.c_str());
 	throw std::runtime_error(errm); //FIXME no! Use. Results.
 }
+#endif
+
+using fd_t = int;
+#ifdef _WIN32
+using ResourceHandle = HANDLE;
+#else
+using ResourceHandle = fd_t;
+#endif
+
+#ifdef _WIN32
+struct IOCompletionInfo {
+	BOOL status;
+	DWORD transferred;
+	DWORD lerr;
+};
+#else
+using IOCompletionInfo = int;
+#endif
 
 class FileResource : public IAIOResource {
 	enum class Mode {
@@ -224,9 +100,20 @@ class FileResource : public IAIOResource {
 	std::weak_ptr<FileResource> slf;
 	IOYengine* engine;
 	Mode mode;
-	fd_t file;
 	std::array<char, DEFAULT_BUFFER_SIZE> buffer;
-	std::shared_ptr<OutsideFuture<int>> engif;
+	std::shared_ptr<OutsideFuture<IOCompletionInfo>> engif;
+	#ifdef _WIN32
+	HANDLE file;
+	#else
+	fd_t file;
+	#endif
+	#ifdef _WIN32
+	struct Trixter {
+		OVERLAPPED overlapped;
+		FileResource* cmon;
+	};
+	Trixter trixter = {};
+	#else
 	bool reged = false;
 	bool lazyEpollReg(){
 		if(reged) return false;
@@ -244,23 +131,70 @@ class FileResource : public IAIOResource {
 		}
 		return reged = true;
 	}
+	#endif
 	public:
 		friend class IOYengine;
-		FileResource(IOYengine* e, Mode m, const std::string& path) : engine(e), mode(m), buffer(), engif(new OutsideFuture<int>()) {
+		FileResource(IOYengine* e, Mode m, const std::string& path) : engine(e), mode(m), buffer(), engif(new OutsideFuture<IOCompletionInfo>()) {
+			#ifdef _WIN32
+			trixter.cmon = this;
+			file = CreateFileA(path.c_str(), mode == Mode::Write ? GENERIC_WRITE : GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_FLAG_OVERLAPPED/* | FILE_FLAG_NO_BUFFERING cf https://docs.microsoft.com/en-us/windows/win32/fileio/file-buffering?redirectedfrom=MSDN */, NULL);
+			if(file == INVALID_HANDLE_VALUE) throw std::runtime_error("CreateFile failed :("); //FIXME no?
+			CreateIoCompletionPort(file, e->ioCompletionPort, COMPLETION_KEY_IO, 0);
+			#else
 			file = open(path.c_str(), (mode == Mode::Write ? O_WRONLY | O_CREAT : O_RDONLY) | O_NONBLOCK | O_CLOEXEC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 			if(file < 0) PrintLastError("Open file failed"); //FIXME no?
+			#endif
 		}
 		FileResource(const FileResource& cpy) = delete;
 		FileResource(FileResource&& mov) = delete;
 		~FileResource(){
+			#ifdef _WIN32
+			if(file != INVALID_HANDLE_VALUE) CloseHandle(file);
+			#else
 			if(file >= 0) close(file);
+			#endif
 		}
 		auto setSelf(std::shared_ptr<FileResource> self){
 			return slf = self;
 		}
 		Future<std::vector<char>> read(unsigned bytes){
+			//self.get() == this   exists to memory-lock dangling IO resource to this lambda generator
 			return defer(lambdagen([this, self = slf.lock(), bytes]([[maybe_unused]] const Yengine* engine, bool& done, std::vector<char>& data) -> std::variant<AFuture, something<std::vector<char>>> {
 				if(done) return data;
+				#ifdef _WIN32
+				if(engif->s == FutureState::Completed){
+					IOCompletionInfo result = *engif->r;
+					if(!result.status){
+						if(result.lerr == ERROR_HANDLE_EOF){
+							done = true;
+							return data;
+						} else PrintLastError(result.lerr);
+					} else {
+						data.insert(data.end(), buffer.begin(), buffer.begin()+result.transferred);
+						trixter.overlapped.Offset += result.transferred;
+						if(bytes > 0 && (done = data.size() >= bytes)){
+							done = true;
+							return data;
+						}
+					}
+				}
+				DWORD transferred = 0;
+				while(ReadFile(file, buffer.begin(), bytes == 0 ? buffer.size() : std::min(buffer.size(), bytes - data.size()), &transferred, &trixter.overlapped)){
+					data.insert(data.end(), buffer.begin(), buffer.begin() + transferred);
+					trixter.overlapped.Offset += transferred;
+					if(bytes > 0 && data.size() >= bytes){
+						done = true;
+						return data;
+					}
+				}
+				switch(::GetLastError()){
+					case ERROR_IO_PENDING: break;
+					case ERROR_HANDLE_EOF:
+						done = true;
+						return data;
+					default: PrintLastError(::GetLastError());
+				}
+				#else
 				if(lazyEpollReg()) return engif;
 				if(engif->s == FutureState::Completed){
 					int leve = *engif->r;
@@ -279,6 +213,7 @@ class FileResource : public IAIOResource {
 					}
 					if(errno != EWOULDBLOCK && errno != EAGAIN) PrintLastError("Read failed");
 				}
+				#endif
 				engif->s = FutureState::Running;
 				return engif;
 			}, std::vector<char>()));
@@ -287,6 +222,26 @@ class FileResource : public IAIOResource {
 			return defer(lambdagen([this, self = slf.lock()]([[maybe_unused]] const Yengine* engine, bool& done, std::vector<char>& data) -> std::variant<AFuture, something<void>> {
 				if(data.empty()) done = true;
 				if(done) return something<void>();
+				#ifdef _WIN32
+				if(engif->s == FutureState::Completed){
+					IOCompletionInfo result = *engif->r;
+					if(!result.status) PrintLastError(result.lerr);
+					else {
+						data.erase(data.begin(), data.begin()+result.transferred);
+						trixter.overlapped.Offset += result.transferred;
+						if(data.empty()){
+							done = true;
+							return something<void>();
+						}
+					}
+				}
+				DWORD transferred = 0;
+				while(WriteFile(file, data.data(), data.size(), &transferred, &trixter.overlapped)){
+					data.erase(data.begin(), data.begin()+transferred);
+					trixter.overlapped.Offset += transferred;
+				}
+				if(::GetLastError() != ERROR_IO_PENDING) PrintLastError(::GetLastError());
+				#else
 				if(lazyEpollReg()) return engif;
 				if(engif->s == FutureState::Completed){
 					int leve = *engif->r;
@@ -301,6 +256,7 @@ class FileResource : public IAIOResource {
 					}
 					if(errno != EWOULDBLOCK && errno != EAGAIN) PrintLastError("Write failed");
 				}
+				#endif
 				engif->s = FutureState::Running;
 				return engif;
 			}, std::move(data)));
@@ -309,6 +265,20 @@ class FileResource : public IAIOResource {
 
 void IOYengine::iothreadwork(){
 	while(true){
+		#ifdef _WIN32
+		IOCompletionInfo inf;
+		ULONG_PTR key;
+		LPOVERLAPPED overl;
+		inf.status = GetQueuedCompletionStatus(ioCompletionPort, &inf.transferred, &key, &overl, INFINITE);
+		if(key == COMPLETION_KEY_SHUTDOWN) break;
+		if(key == COMPLETION_KEY_IO){
+			inf.lerr = GetLastError();
+			auto resource = reinterpret_cast<FileResource::Trixter*>(overl)->cmon;
+			resource->engif->s = FutureState::Completed;
+			resource->engif->r.emplace(inf);
+			engine->notify(std::dynamic_pointer_cast<IFutureT<IOCompletionInfo>>(resource->engif));
+		}
+		#else
 		::epoll_event events[EPOLL_MAX_EVENTS];
 		auto es = ::epoll_wait(ioEpoll, events, EPOLL_MAX_EVENTS, -1);
 		for(int i = 0; i < es; i++)
@@ -320,6 +290,7 @@ void IOYengine::iothreadwork(){
 				resource->engif->r.emplace(eveid);
 				engine->notify(std::dynamic_pointer_cast<IFutureT<void>>(resource->engif));
 			}
+		#endif
 	}
 }
 
@@ -335,5 +306,3 @@ Resource IOYengine::fileOpenWrite(const std::string& path){
 }
 
 }
-
-#endif
