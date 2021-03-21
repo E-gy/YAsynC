118e3ec301284c115b901ceb477e51ebfa877e53
E-gy <balakhelisey@gmail.com>
2021-03-21 19:49:17 +0100

    dis bad

--- src/monoid.hpp
+++ src/monoid.hpp
@@ -5,7 +5,7 @@ template<typename T> struct monoid {
 	monoid() = default;
 	monoid(const T& v) : t(std::forward<T>(v)) {} 
 	monoid(T && v) : t(std::forward<T>(v)) {}
-	operator T&(){ return t; }
+	// operator T&(){ return t; } Disabled due to potential to wreak havoc in innocent use cases.
 	T && move(){ return std::move(t); }
 };
 template<> struct monoid<void> {
