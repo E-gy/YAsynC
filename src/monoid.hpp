ea4733995a9f4f5eb983476c122b470bbc6a8a7f
E-gy <balakhelisey@gmail.com>
2021-03-21 14:52:42 +0100

    oi da me

--- src/monoid.hpp
+++ src/monoid.hpp
@@ -3,8 +3,8 @@
 template<typename T> struct monoid {
 	T t;
 	monoid() = default;
-	monoid(const T& v) : t(std::forward(v)) {} 
-	monoid(T && v) : t(std::forward(v)) {}
+	monoid(const T& v) : t(std::forward<T>(v)) {} 
+	monoid(T && v) : t(std::forward<T>(v)) {}
 	operator T&(){ return t; }
 	T && move(){ return std::move(t); }
 };
